4c7327e62056832f252e26741e879f6919cf8451
Ved Sharanagate <78638686+VortexV7@users.noreply.github.com>
2025-03-07 18:44:28 +0530

    Update app.py
    updated port for hosting on railway

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def upload():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=False)
